8675182ada9a1eda24531da690a7edaa6ba04e2c
haruki-taka8 <stevefestl@gmail.com>
2021-04-11 18:08:14 +0800

    Ahoy! Initial commit!

---
+++ 007-PerpendicularAnagram/007-PerpendicularAnagram.py
@@ -0,0 +1,23 @@
+# 007-Hangman.py
+# > im useless
+#             I   m useless
+#         i   M    useless
+#        im       useless
+#       im    U   seless
+#      im u   S   eless
+#     im us   E   less
+#    im use   L   ess
+#   im usel   E   ss
+#  im usele   S   s
+# im useles   S
+
+import math
+inp = input('> ')
+
+for i in range(len(inp), 0, -1):
+    toPrint = inp
+    sep = ' ' * math.floor(math.sqrt(len(inp)))
+    toPrint = toPrint[:len(inp)-i] + sep + toPrint[len(inp)-i].capitalize() + sep + toPrint[len(inp)-i+1:]
+
+    print(' '*(i-1), end='')
+    print(toPrint)
